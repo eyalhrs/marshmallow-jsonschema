5d9b83b1b7f730174ebe35fde5a4ebed411cf83c
Eyal Hirsch <eyal@Eyals-MacBook-Pro.local>
2018-02-04 22:59:34 +0200

    ignoring auto_id_0

--- marshmallow_jsonschema/base.py
+++ marshmallow_jsonschema/base.py
@@ -9,9 +9,7 @@ from marshmallow.decorators import post_dump
 
 from .validation import handle_length, handle_one_of, handle_range
 
-
 __all__ = ['JSONSchema']
-
 
 TYPE_MAP = {
     dict: {
@@ -68,7 +66,6 @@ TYPE_MAP = {
         'type': 'boolean',
     },
 }
-
 
 FIELD_VALIDATORS = {
     validate.Length: handle_length,
@@ -160,96 +157,18 @@ class JSONSchema(Schema):
         mapping = self._get_default_mapping(obj)
         if field.__class__.__name__ == 'Point':
             schema = {
-                "id": "http://json-schema.org/geojson/geometry.json#",
-                "title": "geometry",
-                "description": "One geometry as defined by GeoJSON",
-                "type": "object",
-                "required": [ "type", "coordinates" ],
-                "oneOf": [
-                    {
-                        "title": "Point",
-                        "properties": {
-                            "type": { "enum": [ "Point" ] },
-                            "coordinates": { "$ref": "#/definitions/position" }
-                        }
-                    },
-                    {
-                        "title": "MultiPoint",
-                        "properties": {
-                            "type": { "enum": [ "MultiPoint" ] },
-                            "coordinates": { "$ref": "#/definitions/positionArray" }
-                        }
-                    },
-                    {
-                        "title": "LineString",
-                        "properties": {
-                            "type": { "enum": [ "LineString" ] },
-                            "coordinates": { "$ref": "#/definitions/lineString" }
-                        }
-                    },
-                    {
-                        "title": "MultiLineString",
-                        "properties": {
-                            "type": { "enum": [ "MultiLineString" ] },
-                            "coordinates": {
-                                "type": "array",
-                                "items": { "$ref": "#/definitions/lineString" }
-                            }
-                        }
-                    },
-                    {
-                        "title": "Polygon",
-                        "properties": {
-                            "type": { "enum": [ "Polygon" ] },
-                            "coordinates": { "$ref": "#/definitions/polygon" }
-                        }
-                    },
-                    {
-                        "title": "MultiPolygon",
-                        "properties": {
-                            "type": { "enum": [ "MultiPolygon" ] },
-                            "coordinates": {
-                                "type": "array",
-                                "items": { "$ref": "#/definitions/polygon" }
-                            }
-                        }
-                    }
-                ],
-                "definitions": {
-                    "position": {
+                "title": "Point",
+                "properties": {
+                    "type": {"enum": ["Point"]},
+                    "coordinates": {
                         "description": "A single position",
                         "type": "array",
                         "minItems": 2,
                         "items": [
-                            { "type": "number" },
-                            { "type": "number" }
+                            {"type": "number"},
+                            {"type": "number"}
                         ],
                         "additionalItems": "false"
-                    },
-                    "positionArray": {
-                        "description": "An array of positions",
-                        "type": "array",
-                        "items": { "$ref": "#/definitions/position" }
-                    },
-                    "lineString": {
-                        "description": "An array of two or more positions",
-                        "allOf": [
-                            { "$ref": "#/definitions/positionArray" },
-                            { "minItems": 2 }
-                        ]
-                    },
-                    "linearRing": {
-                        "description": "An array of four positions where the first equals the last",
-                        "allOf": [
-                            { "$ref": "#/definitions/positionArray" },
-                            { "minItems": 4 },
-                            { "maxItems": 4 }
-                        ]
-                    },
-                    "polygon": {
-                        "description": "An array of linear rings",
-                        "type": "array",
-                        "items": { "$ref": "#/definitions/linearRing" }
                     }
                 }
             }
